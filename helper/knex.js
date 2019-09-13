2009706ca37565de8bca9152cbd9d675fc0af9e4
Dhara <dharababariya34@gmail.com>
2019-09-13 23:26:18 +0530

    done

--- helper/knex.js
+++ helper/knex.js
@@ -1,26 +1,25 @@
-// const knex = require('knex')({
-//     client: 'pg',
-//     version: '7.2',
-//     connection: {
-//         host: '127.0.0.1',
-//         user: 'postgres',
-//         password: 'postgres',
-//         database: 'instructor_report'
-//     }
-// });
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host: 'instructor-assignment-report.cnwis3jkhsul.us-east-2.rds.amazonaws.com',
+        user: 'postgres',
+        password: 'postgres',
+        database: 'instructor_report'
+    }
+});
 // module.exports = knex;
 
-const knex = require('knex')({
+// const knex = require('knex')({
 
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    ssl: true,
-    debug: false
-  })
+//     client: 'pg',
+//     connection: process.env.DATABASE_URL,
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     ssl: true,
+//     debug: false
+//   })
 
 
 //expose knex connection object;
