7d2bcd8eae7c7d83f4bf7494f98c1ebff4c42eff
Dhara <dharababariya34@gmail.com>
2019-09-23 18:19:42 +0530

    send res

--- routes/lecturer_class_report.js
+++ routes/lecturer_class_report.js
@@ -16,7 +16,7 @@ router.post('/api/csv_upload', upload.single('myfile'), async (req, res, next) =
         //csv file uplode
 
         const csv_file = String(req.file.buffer);
-        console.log(1)
+       
 
         //content data
 
@@ -26,26 +26,21 @@ router.post('/api/csv_upload', upload.single('myfile'), async (req, res, next) =
             },
             derived: {}
         };
-        console.log(2)
+        
 
         //covert csv to json
 
         const new_users = await csv_to_json(data);
 
         // console.log(new_users);
-        console.log(3)
-
-        res
-            .status(200)
-            .send({ status: 'SUCCESS', message: 'Successfully Added File ✅️' })
+     
+        
         // .json(data);
         // take one data in new_user
         let lecturer_id = 0;
         
         for (let i = 0; i < new_users.length; i++) {
             // if(i > 1) continue;
-
-            const trx = await knex.transaction();
 
             const add_users = new_users[i];
             // console.log(add_users.Class_Date);
@@ -55,41 +50,16 @@ router.post('/api/csv_upload', upload.single('myfile'), async (req, res, next) =
                 // const result = await knex("public.authors")
                 // .select("*")
                 // .where("name", '=', req.query.name);
-                // lecturer_id =  result[0].id
+                // lecturer_id =  result[0].i
 
 
-                trx('public.lecture')
-                .insert({name: add_users.class_Date})
-                .returning('id')
-                .then(trx.commit)
-                .catch(trx.rollback);
-
-
-                // let inserted_id = await knex("public.lecture")
-                //     .insert({
-                //         name: add_users.Class_Date
-                //     }).returning('id');
-                lecturer_id = parseInt(id.toString());
+                let inserted_id = await knex("public.lecture")
+                    .insert({
+                        name: add_users.Class_Date
+                    }).returning('id');
+                lecturer_id = parseInt(inserted_id.toString());
 
             }
-
-            console.log(4)
-
-            trx('public.lecturer_class_report')
-            .insert({
-                class_date: add_users.Class_Date,
-                section: add_users.Section,
-                school_or_customer_name_room: add_users.School_or_Customer_Name_Room,
-                zone: add_users.Zone,
-                program: add_users.Program,
-                time_range: add_users.Time_Range,
-                kit_take_home: add_users.Kit_Take_Home,
-                role: add_users.Role,
-                reg: add_users.Regd,
-                lecturer_id: lecturer_id
-            })
-            .then(trx.commit)
-            .catch(trx.rollback);
 
             const result = await knex("public.lecturer_class_report")
                 .insert({
@@ -105,14 +75,15 @@ router.post('/api/csv_upload', upload.single('myfile'), async (req, res, next) =
                     lecturer_id: lecturer_id
                 }).returning('*');
 
-            console.log(5)
             // if(i === 25)
-
+            res
+            .status(200)
+            .send({ status: 'SUCCESS', message: 'Successfully Added File ✅️' })
 
             // break;
 
         }
-        console.log(6)
+       
 
         return
 
