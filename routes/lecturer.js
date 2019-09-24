77f94a608bb17991e74a714d98abc23ff4e3b540
Dhara <dharababariya34@gmail.com>
2019-09-24 11:55:41 +0530

    find lecturer name

--- routes/lecturer.js
+++ routes/lecturer.js
@@ -7,20 +7,32 @@ const knex = require('../helper/knex');
 // get lecturer wise class
 
 router.get('/api/get_users', async(req, res ) => {
+    try {
+        
+    
 
     const result = await knex("public.lecture")
         .select("*")
         .where("name", '=', req.query.name);
 
-        // console.log(result[0].id);
+       console.log(result[0].id);
 
     const resultDetail = await knex("public.lecturer_class_report")
     .where("lecturer_id", '=',result[0].id);
+
+
+
+    console.log(resultDetail)
 
     return res
         .status(200)
         .send({status: 'SUCCESS', resultDetail});
 
+
+    } catch (error) {
+        console.error(error)
+        
+    }
 })
 
 module.exports = router;
